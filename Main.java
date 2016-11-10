2649bb3070c643136dc298d133f4bbaff9bd0a2a
Dandello <Artgildin@gmail.com>
2016-11-10 23:03:19 +0300

    Second commit

--- Main.java
+++ Main.java
@@ -6,10 +6,13 @@ public class Main {
         TerminalServer terminalServer = new TerminalServer();
         PinValidator pinValidator = new PinValidator();
         Terminal terminal = new Terminal(terminalServer, pinValidator);
+        try {
+            terminal.login("1234");
+            terminal.getMoney(4000);
+            System.out.print(terminal.checkAccountStatus());
 
-        terminal.login("1234");
-        terminal.putMoney(100);
-        System.out.print(terminal.checkAccountStatus());
-
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
